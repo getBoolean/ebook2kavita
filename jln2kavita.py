16bad8c994e2cf6e8259d5c9fabaa49db99cb740
getBoolean <danielwnewville@gmail.com>
2023-02-19 16:21:15 -0600

    Fix lints

--- jln2kavita.py
+++ jln2kavita.py
@@ -1,10 +1,14 @@
+'''
+Script to convert a JLN epub directory structure to a Kavita directory structure.
+'''
+
 import os
 import sys
 import shutil
 import argparse
 import re
-from tqdm import tqdm
 import subprocess
+from tqdm import tqdm
 
 def set_epub_series_and_index(epub_file_path, series_title, series_index):
     # Construct the command
@@ -14,7 +18,7 @@ def set_epub_series_and_index(epub_file_path, series_title, series_index):
         command += ['--index', str(series_index)]
 
     # Run the command
-    result = subprocess.run(command, capture_output=True)
+    result = subprocess.run(command, capture_output=True, check=False)
 
     # Check the output for errors
     if result.returncode != 0:
@@ -41,7 +45,7 @@ def extract_volume_number(string):
         if match:
             try:
                 return match.group(1)
-            except:
+            except IndexError:
                 return match.group(0)
     return None
 
@@ -77,8 +81,10 @@ def copy_epub_files(src_dir, dest_dir):
         if not official_folder_found:
             for series_sub_folder in os.listdir(series_folder_path):
                 series_sub_folder_path = os.path.join(series_folder_path, series_sub_folder)
-                if os.path.isdir(series_sub_folder_path) and 'official' in series_sub_folder.lower():
-                    epub_temp_path = os.path.join(series_sub_folder_path, 'EPUB');
+                if os.path.isdir(series_sub_folder_path) \
+                    and 'official' in series_sub_folder.lower():
+
+                    epub_temp_path = os.path.join(series_sub_folder_path, 'EPUB')
                     if os.path.exists(epub_temp_path):
                         epub_folder_path = epub_temp_path
                     else:
@@ -86,34 +92,48 @@ def copy_epub_files(src_dir, dest_dir):
                     official_folder_found = True
                     break
 
-        # if not official_folder_found:
-        #     print('Warning: Official folder not found for {}. Using EPUB folder.'.format(series_folder))
-
         dest_series_folder = os.path.join(dest, series_folder)
         if not os.path.exists(dest_series_folder):
             os.makedirs(dest_series_folder)
 
-        epub_files = [os.path.join(epub_folder_path, f) for f in os.listdir(epub_folder_path) if os.path.isfile(os.path.join(epub_folder_path, f)) and f.lower().endswith('.epub')]
+        epub_files = [os.path.join(epub_folder_path, f)
+                      for f in os.listdir(epub_folder_path)
+                        if os.path.isfile(os.path.join(epub_folder_path, f))
+                            and f.lower().endswith('.epub')]
 
         if not epub_files:
             continue
 
-        print('{}:'.format(series_folder))
-        with tqdm(total=len(epub_files)) as pbar:
-            for epub_file in epub_files:
-                dest_epub = os.path.join(dest_series_folder, os.path.basename(epub_file))
-                if os.path.exists(dest_epub):
-                    pbar.update(1)
-                    continue
-
-                # Use calibre-meta to set the series and index
-                temp_epub_file = dest_epub + ".temp.epub"
-                shutil.copy(epub_file, temp_epub_file)
-                vol_num = extract_volume_number(os.path.basename(epub_file))
-                set_epub_series_and_index(temp_epub_file, series_folder, vol_num)
-                shutil.move(temp_epub_file, dest_epub)
-                
+        print(f'{series_folder}:')
+        pbar = tqdm(epub_files)
+        for epub_file in pbar:
+            dest_epub = os.path.join(dest_series_folder, os.path.basename(epub_file))
+            if os.path.exists(dest_epub):
                 pbar.update(1)
+                continue
+
+            # Use calibre-meta to set the series and index
+            temp_epub_file = dest_epub + ".temp.epub"
+            shutil.copy(epub_file, temp_epub_file)
+            vol_num = extract_volume_number(os.path.basename(epub_file))
+            set_epub_series_and_index(temp_epub_file, series_folder, vol_num)
+            shutil.move(temp_epub_file, dest_epub)
+
+        # with tqdm(total=len(epub_files)) as pbar:
+        #     for epub_file in epub_files:
+        #         dest_epub = os.path.join(dest_series_folder, os.path.basename(epub_file))
+        #         if os.path.exists(dest_epub):
+        #             pbar.update(1)
+        #             continue
+
+        #         # Use calibre-meta to set the series and index
+        #         temp_epub_file = dest_epub + ".temp.epub"
+        #         shutil.copy(epub_file, temp_epub_file)
+        #         vol_num = extract_volume_number(os.path.basename(epub_file))
+        #         set_epub_series_and_index(temp_epub_file, series_folder, vol_num)
+        #         shutil.move(temp_epub_file, dest_epub)
+
+        #         pbar.update(1)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Copy EPUB files from one directory to another')
