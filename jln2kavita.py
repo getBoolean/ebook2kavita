ae830e5e792ace5faccb15ea3d3efa3df5260618
getBoolean <danielwnewville@gmail.com>
2023-02-21 19:43:03 -0600

    Add support for part directories and refactoring

--- jln2kavita.py
+++ jln2kavita.py
@@ -11,15 +11,47 @@ import subprocess
 from tqdm import tqdm
 
 
-def set_epub_series_and_index(epub_file_path, series_title, series_index):
+VOLUME_PATTERNS: list = [
+    re.compile(r'v[\s.-]*(\d+(\.\d+)?)', re.IGNORECASE),
+    re.compile(r'vol[\s.-]*(\d+(\.\d+)?)', re.IGNORECASE),
+    re.compile(r'volume[\s.-]*(\d+(\.\d+)?)', re.IGNORECASE),
+    re.compile(r'LN[\s]*(\d+(\.\d+)?)', re.IGNORECASE),
+]
+
+BACKUP_VOLUME_PATTERN = re.compile(r'\b\d+(?:\.\d+)?\b')
+
+PART_PATTERNS: list = [
+    re.compile(r'part[\s.-]*(\d+(\.\d+)?)', re.IGNORECASE),
+    re.compile(r'pt[\s.-]*(\d+(\.\d+)?)', re.IGNORECASE),
+]
+
+
+def find_part_folders(series_folder_path: str) -> list[str]:
+    '''Find the part folders in the given folder
+    '''
+    part_folders = []
+    for series_sub_folder in os.listdir(series_folder_path):
+        series_sub_folder_path = os.path.join(
+            series_folder_path, series_sub_folder)
+        if os.path.isdir(series_sub_folder_path) and 'part' in series_sub_folder.lower():
+            part_folders.append(series_sub_folder_path)
+    return part_folders
+
+
+def set_epub_series_and_index(epub_file_path: str, series_title: str,
+                              part_num: str | None, series_index: str | None) -> None:
     '''
     Set the series and index of an epub file using calibre.
     '''
     # Construct the command
-    command = ['ebook-meta', epub_file_path, '--series', series_title]
+    title: str = series_title
+    if part_num:
+        title = f'{series_title} Part {part_num}'
+
+    command = ['ebook-meta', epub_file_path, '--series', title]
 
     if series_index is not None:
-        command += ['--index', str(series_index)]
+        command += ['--index', series_index]
 
     # Run the command
     result = subprocess.run(command, capture_output=True, check=False)
@@ -29,28 +61,119 @@ def set_epub_series_and_index(epub_file_path, series_title, series_index):
         print('Error:', result.stderr.decode('utf-8'))
 
 
-VOLUME_PATTERNS = [
-    re.compile(r't[\s.-]*(\d+(\.\d+)?)', re.IGNORECASE),
-    re.compile(r'v[\s.-]*(\d+(\.\d+)?)', re.IGNORECASE),
-    re.compile(r'vol[\s.-]*(\d+(\.\d+)?)', re.IGNORECASE),
-    re.compile(r'volume[\s.-]*(\d+(\.\d+)?)', re.IGNORECASE),
-    re.compile(r'LN[\s]*(\d+(\.\d+)?)', re.IGNORECASE),
-    re.compile(r'\b\d+(?:\.\d+)?\b'),
-]
-
-
-def extract_volume_number(string):
+def extract_part_number(filename: str) -> str | None:
     '''
-    Extract the volume number from the filename.
+    Extract the part number from the filename.
     '''
-    for pattern in VOLUME_PATTERNS:
-        match = pattern.search(string)
+    for pattern in PART_PATTERNS:
+        match = pattern.search(filename)
         if match:
             try:
                 return match.group(1)
             except IndexError:
                 return match.group(0)
     return None
+
+
+def extract_volume_number(filename: str) -> str | None:
+    '''
+    Extract the volume number from the filename.
+    '''
+
+    for pattern in VOLUME_PATTERNS:
+        match = pattern.search(filename)
+        if match:
+            try:
+                return match.group(1)
+            except IndexError:
+                return match.group(0)
+
+    if not extract_part_number(filename):
+        match = BACKUP_VOLUME_PATTERN.search(filename)
+        if match:
+            try:
+                return match.group(1)
+            except IndexError:
+                return match.group(0)
+
+    return None
+
+
+def find_series_epub_files(series_folder_path: str) -> list[str]:
+    '''
+    Find the epub files in the given series folder, with a preference for the official translations.
+
+    Possible paths:
+
+    - `/Series/Part */EPUB/*.epub`
+    - `/Series/EPUB/*.epub`
+    - `/Series/EPUB/Official/*.epub`
+    - `/Series/Official/EPUB/*.epub`
+    - `/Series/Official/*.epub`
+    - `/Series/*.epub`
+    '''
+    root_official_folder = find_official_folder(series_folder_path)
+    root_part_folders = find_part_folders(series_folder_path)
+    epub_folder_path = os.path.join(series_folder_path, 'EPUB')
+    has_root_epub_folder = os.path.isdir(epub_folder_path)
+    if has_root_epub_folder:
+        # find official subfolder from folder
+        official_folder = find_official_folder(epub_folder_path)
+        if official_folder:
+            epub_folder_path = official_folder
+    elif root_official_folder:
+        official_epub_folder_path = os.path.join(
+            series_folder_path, 'EPUB')
+        has_epub_folder = os.path.isdir(official_epub_folder_path)
+        if has_epub_folder:
+            epub_folder_path = official_epub_folder_path
+        else:
+            epub_folder_path = root_official_folder
+    else:
+        epub_folder_path = series_folder_path
+
+    epub_files = []
+    if root_part_folders:
+        for part_folder in root_part_folders:
+            part_epub_folder_path = os.path.join(part_folder, 'EPUB')
+            has_part_epub_folder = os.path.isdir(part_epub_folder_path)
+            if has_part_epub_folder:
+                # find official subfolder from folder
+                official_folder = find_official_folder(
+                    part_epub_folder_path)
+                if official_folder:
+                    part_epub_folder_path = official_folder
+                epub_files += [os.path.join(part_epub_folder_path, f)
+                               for f in os.listdir(part_epub_folder_path)
+                               if os.path.isfile(os.path.join(part_epub_folder_path, f))
+                               and f.lower().endswith('.epub')]
+            else:
+                epub_files += [os.path.join(part_folder, f)
+                               for f in os.listdir(part_folder)
+                               if os.path.isfile(os.path.join(part_folder, f))
+                               and f.lower().endswith('.epub')]
+
+    else:
+        epub_files += [os.path.join(epub_folder_path, f)
+                       for f in os.listdir(epub_folder_path)
+                       if os.path.isfile(os.path.join(epub_folder_path, f))
+                       and f.lower().endswith('.epub')]
+
+    return epub_files
+
+
+def copy_epub_file(series_folder, epub_file_path, dest_epub_path):
+    '''
+    Copy an epub file from JLN directory to a Kavita directory.
+    '''
+    # Use calibre-meta to set the series and index
+    temp_epub_file = dest_epub_path + ".temp.epub"
+    shutil.copy(epub_file_path, temp_epub_file)
+    vol_num = extract_volume_number(os.path.basename(epub_file_path))
+    part_num = extract_part_number(os.path.basename(epub_file_path))
+    set_epub_series_and_index(
+        temp_epub_file, series_folder, part_num, vol_num)
+    shutil.move(temp_epub_file, dest_epub_path)
 
 
 def copy_epub_files(src_dir, dest_dir):
@@ -73,71 +196,35 @@ def copy_epub_files(src_dir, dest_dir):
         if not os.path.isdir(series_folder_path):
             continue
 
-        epub_folder_path = os.path.join(series_folder_path, 'EPUB')
-
-        # Possible paths:
-        #   TODO: /Series/Part */EPUB/*.epub
-        #   /Series/EPUB/*.epub
-        #   /Series/EPUB/Official/*.epub
-        #   /Series/Official/EPUB/*.epub
-        #   /Series/*.epub
-
-        if not os.path.isdir(epub_folder_path):
-            # Skips Ascendence of a Bookworm
-            continue
-
-        official_folder_found = False
-        for epub_sub_folder in os.listdir(epub_folder_path):
-            epub_sub_folder_path = os.path.join(
-                epub_folder_path, epub_sub_folder)
-            if os.path.isdir(epub_sub_folder_path) and 'official' in epub_sub_folder.lower():
-                epub_folder_path = epub_sub_folder_path
-                official_folder_found = True
-                break
-
-        if not official_folder_found:
-            for series_sub_folder in os.listdir(series_folder_path):
-                series_sub_folder_path = os.path.join(
-                    series_folder_path, series_sub_folder)
-                if os.path.isdir(series_sub_folder_path) \
-                        and 'official' in series_sub_folder.lower():
-
-                    epub_temp_path = os.path.join(
-                        series_sub_folder_path, 'EPUB')
-                    if os.path.exists(epub_temp_path):
-                        epub_folder_path = epub_temp_path
-                    else:
-                        epub_folder_path = series_sub_folder_path
-                    official_folder_found = True
-                    break
-
         dest_series_folder = os.path.join(dest, series_folder)
         if not os.path.exists(dest_series_folder):
             os.makedirs(dest_series_folder)
 
-        epub_files = [os.path.join(epub_folder_path, f)
-                      for f in os.listdir(epub_folder_path)
-                      if os.path.isfile(os.path.join(epub_folder_path, f))
-                      and f.lower().endswith('.epub')]
-
-        if not epub_files:
+        epub_file_paths = find_series_epub_files(series_folder_path)
+        if not epub_file_paths:
             continue
 
         print(f'{series_folder}:')
-        pbar = tqdm(epub_files)
-        for epub_file in pbar:
-            dest_epub = os.path.join(
-                dest_series_folder, os.path.basename(epub_file))
-            if os.path.exists(dest_epub):
+        pbar = tqdm(epub_file_paths)
+        for epub_file_path in pbar:
+            dest_epub_path = os.path.join(
+                dest_series_folder, os.path.basename(epub_file_path))
+            if os.path.exists(dest_epub_path):
                 pbar.update(1)
                 continue
 
-            # Use calibre-meta to set the series and index
-            temp_epub_file = dest_epub + ".temp.epub"
-            shutil.copy(epub_file, temp_epub_file)
-            vol_num = extract_volume_number(os.path.basename(epub_file))
-            set_epub_series_and_index(temp_epub_file, series_folder, vol_num)
-            shutil.move(temp_epub_file, dest_epub)
+            copy_epub_file(series_folder, epub_file_path, dest_epub_path)
+
+
+def find_official_folder(epub_folder_path) -> str | None:
+    '''Find the official folder in the given folder, or None if it does not exist
+    '''
+    for epub_sub_folder in os.listdir(epub_folder_path):
+        epub_sub_folder_path = os.path.join(
+            epub_folder_path, epub_sub_folder)
+        if os.path.isdir(epub_sub_folder_path) and 'official' in epub_sub_folder.lower():
+            return epub_sub_folder_path
+    return None
 
 
 if __name__ == '__main__':
